789fa20df200fe4dd94b7de844ad6adc36d0f0fb
JT Smrdel <jtsmrdel@gmail.com>
2024-03-02 13:36:39 -0500

    Removed boilerplate and renamed App

---
+++ Morse Messenger/Morse Messenger/MorseMessenger.swift
@@ -0,0 +1,19 @@
+//
+//  MorseMessenger.swift
+//  Morse Messenger
+//
+//  Created by JT Smrdel on 2/27/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MorseMessenger: App {
+
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
